2e3624db042650c47c41718ee16021d60e47b86e
Katch Vidal <katchvidal@gmail.com>
2022-06-12 20:21:41 -0500

    feature: formulario por template basico

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild( 'f' ) f!: NgForm;
+
+  initialForm = {
+    nombre: 'RTX 3060 ti',
+    precio: 0,
+    existencias : 0
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  //  Funcion para chequear nombre valido en el formulario
+  nombreValid() : boolean {
+    return this.f?.controls['nombre']?.invalid && this.f.controls['nombre']?.touched
+  }
+
+  precioValid() : boolean {
+    return this.f?.controls['precio']?.touched && this.f?.controls['precio']?.value <= 0
+  }
+  existenciasValid() : boolean {
+    return this.f?.controls['existencias']?.touched && this.f?.controls['existencias']?.value <= 0
+  }
+
+  onSubmit(  ){
+    // console.log( this.f.value.precio );
+    console.log('Posteo Formulario Correctamente');
+    this.f.resetForm();
+  }
+
+} 
--- src/app/template/basicos/basicos.module.ts
+++ src/app/template/basicos/basicos.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { BasicosRoutingModule } from './basicos-routing.module';
 import { BasicosComponent } from './basicos.component';
+import { FormsModule } from '@angular/forms';
+import { CustomMinDirective } from '../directivas/custom-min.directive';
 
 
 @NgModule({
   declarations: [
-    BasicosComponent
+    BasicosComponent,
+    CustomMinDirective,
   ],
   imports: [
+    FormsModule,
     CommonModule,
     BasicosRoutingModule
   ]
